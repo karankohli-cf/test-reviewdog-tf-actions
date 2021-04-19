e90431929f8f21fc5d097f4557976fd7b1b89dc5
Vlad Ionescu <Vlaaaaaaad@users.noreply.github.com>
2021-04-19 23:39:22 +0300

    Update tfsec.tf (#2)
    * Update tfsec.tf
    
    * Dir?
    
    * I iz cranky now
    
    * HAHAA!
    
    * 1.6.0
    
    * So confused
    
    * /:)

--- tfsec.tf
+++ tfsec.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "example" {
 
 resource "aws_security_group_rule" "example_rule" {
   security_group_id = aws_security_group.example.id
-  description       = "Allow all traffic from Internet which will make tfsec cranky"
+  description       = "Allow all traffic from Internet which will make tfsec soooo cranky"
 
   type      = "ingress"
   from_port = "0"
