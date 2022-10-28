293a763a6180a06c68cd2ddb2e7d6768048a6bd6
Karan Kohli <karan.kohli@contentful.com>
2022-10-28 14:29:32 +0200

    block

--- kics.tf
+++ kics.tf
@@ -9,7 +9,8 @@ resource "aws_security_group" "web-node" {
     to_port   = 80
     protocol  = "tcp"
     cidr_blocks = [
-    "0.0.0.0/0"]
+      "0.0.0.0/0"
+    ]
   }
   ingress {
     from_port = 22
