a79b2166b183cd599fb9a2c6c412b32e9bcec6c9
Karan Kohli <karan.kohli@contentful.com>
2022-10-28 14:09:01 +0200

    add vuln

--- kics.tf
+++ kics.tf
@@ -4,4 +4,26 @@ resource "aws_security_group" "web-node" {
   description = "${local.resource_prefix.value} Security Group"
   vpc_id      = aws_vpc.web_vpc.id
 
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
 }
